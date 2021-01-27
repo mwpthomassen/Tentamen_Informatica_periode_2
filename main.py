3a56427d73c268309d36f98b0daaaa9202e0ca0d
mwpthomassen <74045335+mwpthomassen@users.noreply.github.com>
2021-01-27 23:49:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-# leest het fasta bestand in
+# leest het fasta bestand in en zet het in een lijst
 def inlezen_fasta_file(bestandsnaam):
     with open(bestandsnaam) as inFile:
+        Proteine = []
         for regel in inFile:
-            regel.split("")
-            print(regel)
+            if not regel.startswith(">"):
+                print(regel)
+
+
+
 
 
 if __name__ == '__main__':
